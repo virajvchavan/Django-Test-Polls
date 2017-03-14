7b77ffeeebb86cee0e09e1b73918915ffff26b9b
Viraj <virajc@live.com>
2017-03-14 19:02:49 +0530

    Part 3 completed

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
-
 admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
-import datetime
-
 from django.db import models
+
+import datetime
 from django.utils import timezone
 
 # Create your models here.
